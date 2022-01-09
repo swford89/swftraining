ca544c70d11d9259398ea5999c06240014f4d1d6
Scott Ford <swford@ualberta.ca>
2022-01-08 21:48:34 -0700

    created env variable for SECRET_KEY

--- swftraining/settings.py
+++ swftraining/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d8)@5%1ji3#itpk%@8l0n6g$us4axyd^1_00tj6es1lqn(s*ij'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
